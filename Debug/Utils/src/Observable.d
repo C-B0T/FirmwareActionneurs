b027e7c55bd08f7cd16c55cecf9f726532d45288
3Xpresso <jeremy.roulland@gmail.com>
2017-05-07 19:03:46 +0200

    Remove Debug/ folder and cleanup .cprojet

--- Debug/Utils/src/Observable.d
+++
@@ -1,7 +0,0 @@
-Utils/src/Observable.o: ../Utils/src/Observable.cpp \
- C:/Users/Bureau/STM32Workspace/FirmwareActionneurs/Utils/inc/Observable.hpp \
- C:/Users/Bureau/STM32Workspace/FirmwareActionneurs/Application/inc/common.h
-
-C:/Users/Bureau/STM32Workspace/FirmwareActionneurs/Utils/inc/Observable.hpp:
-
-C:/Users/Bureau/STM32Workspace/FirmwareActionneurs/Application/inc/common.h:
