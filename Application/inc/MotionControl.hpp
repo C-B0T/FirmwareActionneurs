320b4bfd7d2fb9343d389566076ebb3538c55e6b
3Xpresso <jeremy.roulland@gmail.com>
2017-05-17 23:52:15 +0200

    Integration MotionControl

---
+++ Application/inc/MotionControl.hpp
@@ -0,0 +1,224 @@
+/**
+ * @file    MotionControl.hpp
+ * @author    Jeremy ROULLAND
+ * @date    3 jan. 2017
+ * @brief    MotionControl namespace include file
+ */
+
+#ifndef INC_MOTIONCONTROL_HPP_
+#define INC_MOTIONCONTROL_HPP_
+
+/**
+ * @namespace MotionControl
+ * @brief motion controller objects
+ *
+ * Contains class such as PositionControl, VelocityControl, MotionProfile, etc.
+ */
+
+#include "common.h"
+
+// For std::string
+#include <algorithm>
+
+
+#include "Odometry.hpp"
+#include "PositionControlStepper.hpp"
+#include "ProfileGenerator.hpp"
+#include "TrajectoryPlanning.hpp"
+
+// FreeRTOS
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "task.h"
+#include "timers.h"
+
+using namespace Location;
+
+typedef enum
+{
+    CMD_ID_UNKNOWN                =    -1,
+//    CMD_TYPE_CHECKUP            =    0x10,
+//    CMD_ID_GET_POSITION            =    0x20,
+    CMD_ID_GET_ANGLE            =    0x21,
+    CMD_ID_GOTO                    =    0x40,
+    CMD_ID_GOLIN                =    0x41,
+    CMD_ID_GOANG                =    0x42,
+    CMD_ID_SET_POSITION            =    0x50,
+    CMD_ID_SET_ANGLE            =    0x51,
+//    CMD_ID_STOP                    =    0x60,
+}CMD_TYPE;
+
+struct cmd_t
+{
+    CMD_TYPE id;
+    union
+    {
+        float32_t d;
+        float32_t a;
+        struct {
+        	float32_t x;
+        	float32_t y;
+        }xy;
+    }data;
+};
+
+namespace MotionControl
+{
+
+    /**
+     * @class FBMotionControl
+     * @brief Feedback Controller class
+     *
+     * HOWTO :
+     * -
+     *
+     */
+    class FBMotionControl
+    {
+    public:
+        /**
+         * @brief Get instance method
+         * @return VelocityLoop instance
+         */
+        static FBMotionControl* GetInstance();
+
+        /**
+         * @brief Return instance name
+         */
+        std::string Name()
+        {
+            return this->name;
+        }
+
+        uint16_t GetStatus()
+        {
+            return this->status;
+        }
+
+        void Enable();
+
+        void Disable();
+
+        void DisableSafeguard()
+        {
+            this->safeguard = true;
+        }
+        void EnableSafeguard()
+        {
+            this->safeguard = false;
+        }
+        void ToggleSafeguard()
+        {
+            this->safeguard = !this->safeguard;
+        }
+        bool GetSafeguard()
+        {
+            return this->safeguard;
+        }
+
+        // MotionControl Commands
+        void GoLin(int32_t d)
+        {
+            struct cmd_t cmd;
+
+            cmd.id = CMD_ID_GOLIN;
+            cmd.data.d = ((float32_t)d)/1000.0;
+
+            xQueueSend(this->Qorders, (void*) &cmd, 0);
+        }
+
+        void GoAng(int32_t a)
+        {
+        	struct cmd_t cmd;
+
+            cmd.id = CMD_ID_GOANG;
+            cmd.data.a = ((float32_t)a)/10.0/180.0*_PI_;
+
+            xQueueSend(this->Qorders, (void*) &cmd, 0);
+        }
+
+        void Goto(int32_t X, int32_t Y)
+        {
+        	struct cmd_t cmd;
+
+            cmd.id = CMD_ID_GOTO;
+            cmd.data.xy.x = ((float32_t)X)/1000.0;
+            cmd.data.xy.y = ((float32_t)Y)/1000.0;
+
+            xQueueSend(this->Qorders, (void*) &cmd, 0);
+        }
+
+        void Stop()
+        {
+            xQueueReset(this->Qorders);
+            this->tp->stop();
+        }
+
+    protected:
+        FBMotionControl();
+
+        // 16 Flags Status
+        uint16_t status;
+
+        /**
+         * @protected
+         * @brief Instance name
+         */
+        std::string name;
+
+        Odometry           *odometry;
+        PositionControl    *pc;
+        ProfileGenerator   *pg;
+        TrajectoryPlanning *tp;
+
+
+        bool enable;
+
+        bool safeguard;
+
+        struct {
+            bool newOrder;
+            uint8_t id;
+            int32_t X;
+            int32_t Y;
+        } order;
+
+        /**
+         * @brief Compute motion control
+         */
+        void Compute(float32_t period);
+
+        /**
+         * @brief Compute motion control
+         */
+        void Test();
+
+        /**
+         * @protected
+         * @brief Semaphore order
+         *
+         * Used to set order
+         */
+        SemaphoreHandle_t mutex;
+
+        QueueHandle_t Qorders;
+
+        /**
+         * @protected
+         * @brief OS Task handle
+         *
+         * Used by speed control loop
+         */
+        TaskHandle_t taskHandle;
+
+        /**
+         * @protected
+         * @brief Speed control loop task handler
+         * @param obj : Always NULL
+         */
+        void taskHandler (void* obj);
+    };
+
+}
+
+#endif /* INC_MOTIONCONTROL_HPP_ */
---
+++ Application/src/MotionControl.cpp
@@ -0,0 +1,218 @@
+/**
+ * @file    MotionControl.cpp
+ * @author  Jeremy ROULLAND
+ * @date    17 apr. 2017
+ * @brief   Motion Control class
+ */
+
+#include "MotionControl.hpp"
+
+/*----------------------------------------------------------------------------*/
+/* Definitions                                                                */
+/*----------------------------------------------------------------------------*/
+
+#define MC_TASK_STACK_SIZE          (256u)
+#define MC_TASK_PRIORITY            (configMAX_PRIORITIES-3)
+
+#define MC_TASK_PERIOD_MS           (5u)
+#define TP_TASK_PERIOD_MS           (100u)
+#define PG_TASK_PERIOD_MS           (10u)
+#define PC_TASK_PERIOD_MS           (10u)
+#define VC_TASK_PERIOD_MS           (5u)
+
+/*----------------------------------------------------------------------------*/
+/* Private Members                                                            */
+/*----------------------------------------------------------------------------*/
+
+static MotionControl::FBMotionControl* _motionControl = NULL;
+
+/*----------------------------------------------------------------------------*/
+/* Private Functions                                                          */
+/*----------------------------------------------------------------------------*/
+
+namespace MotionControl
+{
+
+    FBMotionControl* FBMotionControl::GetInstance()
+    {
+        // If MotionControl instance already exists
+        if(_motionControl != NULL)
+        {
+            return _motionControl;
+        }
+        else
+        {
+            _motionControl = new FBMotionControl();
+            return _motionControl;
+        }
+    }
+
+    FBMotionControl::FBMotionControl()
+    {
+        this->name = "MotionControl";
+        this->taskHandle = NULL;
+
+        // Set 16 Flags Status
+        this->status = 0x0000;
+
+        // MotionControl is enabled by default
+        this->enable = true;
+
+        // MotionControl Safeguard is enabled by default
+        this->safeguard = true;
+
+        // Odometry instance created in standalone mode
+        this->odometry = Odometry::GetInstance(true);
+
+        // VC, PC instances creations
+        this->pc = PositionControl::GetInstance(false);
+        this->pg = ProfileGenerator::GetInstance(false);
+        this->tp = TrajectoryPlanning::GetInstance(false);
+
+        // Create task
+        xTaskCreate((TaskFunction_t)(&FBMotionControl::taskHandler),
+                    this->name.c_str(),
+                    MC_TASK_STACK_SIZE,
+                    NULL,
+                    MC_TASK_PRIORITY,
+                    NULL);
+
+        this->mutex = xSemaphoreCreateMutex();
+
+        this->Qorders = xQueueCreate(10, sizeof(cmd_t));
+    }
+
+    void FBMotionControl::Enable()
+    {
+    	this->enable = true;
+    	this->pc->Enable();
+    }
+
+    void FBMotionControl::Disable()
+    {
+    	this->enable = false;
+    	this->pc->Disable();
+    }
+
+    void FBMotionControl::Test()
+    {
+        static uint32_t localTime = 0;
+
+        // Schedule MotionControl
+        localTime += MC_TASK_PERIOD_MS;
+
+        // #1 Compute TrajectoryPlanning
+        if((localTime % TP_TASK_PERIOD_MS) == 0)
+            this->tp->Compute(TP_TASK_PERIOD_MS);
+
+        // #2 Schedule PositionControl
+        if((localTime % PC_TASK_PERIOD_MS) == 0)
+            this->pc->Compute(PC_TASK_PERIOD_MS);
+
+        // #3 Schedule ProfileGenerator
+        if((localTime % PG_TASK_PERIOD_MS) == 0)
+            this->pg->Compute(PG_TASK_PERIOD_MS);
+    }
+
+    void FBMotionControl::Compute(float32_t period)
+    {
+        static uint32_t localTime = 0;
+        cmd_t cmd;
+
+        // Update configuration & state status
+        if(this->enable)
+        	this->status |= (1<<0);
+        else
+        	this->status &= ~(1<<0);
+
+        if(this->safeguard)
+        	this->status |= (1<<1);
+        else
+        	this->status &= ~(1<<1);
+
+        if(this->tp->isFinished())
+            this->status |= (1<<8);
+        else
+            this->status &= ~(1<<8);
+
+        // Schedule MotionControl
+        localTime += MC_TASK_PERIOD_MS;
+
+        // If MotionControl is disabled then don't schedule submodules
+        if(this->enable == false)
+            return;
+
+        // #1 Schedule TrajectoryPlanning
+        if((localTime % TP_TASK_PERIOD_MS) == 0)
+        {
+            // 1- Pull orders one by one
+            if(this->tp->isFinished())
+            {
+                if(xQueueReceive(this->Qorders, &cmd, 0) == pdTRUE)
+                {
+                    switch (cmd.id)
+                    {
+                    case CMD_ID_GOLIN:
+                        this->tp->goLinear(cmd.data.d);
+                        break;
+                    case CMD_ID_GOANG:
+                        this->tp->goAngular(cmd.data.a);
+                        break;
+                    case CMD_ID_GOTO:
+                        this->tp->gotoXY(cmd.data.xy.x, cmd.data.xy.y);
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+            // 2- Compute TrajectoryPlanning
+            this->tp->Compute((period * TP_TASK_PERIOD_MS) / MC_TASK_PERIOD_MS);
+        }
+
+        // #2 Schedule PositionControl
+        if((localTime % PC_TASK_PERIOD_MS) == 0)
+            this->pc->Compute((period * PC_TASK_PERIOD_MS) / MC_TASK_PERIOD_MS);
+
+        // #3 Schedule ProfileGenerator
+        if((localTime % PG_TASK_PERIOD_MS) == 0)
+            this->pg->Compute((period * PG_TASK_PERIOD_MS) / MC_TASK_PERIOD_MS);
+    }
+
+
+    void FBMotionControl::taskHandler(void* obj)
+    {
+        TickType_t xLastWakeTime;
+        TickType_t xFrequency = pdMS_TO_TICKS(MC_TASK_PERIOD_MS);
+
+        FBMotionControl* instance = _motionControl;
+        TickType_t prevTick = 0u,  tick = 0u;
+
+        float32_t period = 0.0f;
+
+        // 1. Initialise periodical task
+        xLastWakeTime = xTaskGetTickCount();
+
+        // 2. Get tick count
+        prevTick = xTaskGetTickCount();
+
+        while(1)
+        {
+            // 2. Wait until period elapse
+            vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+            // 3. Get tick
+            tick = xTaskGetTickCount();
+
+            period = static_cast<float32_t>(tick) -
+                     static_cast<float32_t>(prevTick);
+
+            //4. Compute velocity (MotionControl)
+            instance->Compute(period);
+            //instance->Test();
+
+            // 5. Set previous tick
+            prevTick = tick;
+        }
+    }
+}
